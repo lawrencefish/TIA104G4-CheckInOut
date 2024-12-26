a139cf3c2ba545ca1a83daecb3e1a3bb03e96ded
Tom <lawrencefish35@gmail.com>
2024-12-26 17:18:25 +0800

    暫時註解filter

--- src/main/java/com/business/config/FilterConfig.java
+++ src/main/java/com/business/config/FilterConfig.java
@@ -1,19 +1,19 @@
-package com.business.config;
-
-import com.business.filter.LoginFilter;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class FilterConfig {
-
-    @Bean
-    public FilterRegistrationBean<LoginFilter> loginFilter() {
-        FilterRegistrationBean<LoginFilter> registrationBean = new FilterRegistrationBean<>();
-        registrationBean.setFilter(new LoginFilter());
-        registrationBean.addUrlPatterns("/*"); // 设置过滤的 URL 模式
-        registrationBean.setOrder(1); // 优先级，越小越高
-        return registrationBean;
-    }
-}
+//package com.business.config;
+//
+//import com.business.filter.LoginFilter;
+//import org.springframework.boot.web.servlet.FilterRegistrationBean;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class FilterConfig {
+//
+//    @Bean
+//    public FilterRegistrationBean<LoginFilter> loginFilter() {
+//        FilterRegistrationBean<LoginFilter> registrationBean = new FilterRegistrationBean<>();
+//        registrationBean.setFilter(new LoginFilter());
+//        registrationBean.addUrlPatterns("/*"); // 设置过滤的 URL 模式
+//        registrationBean.setOrder(1); // 优先级，越小越高
+//        return registrationBean;
+//    }
+//}
