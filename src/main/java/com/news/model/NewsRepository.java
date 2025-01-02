539b08a87a04fdc5300d03fd7f35719ed1fcaec1
Enzo Pan <enzopan1109@gmail.com>
2025-01-02 16:23:19 +0800

    news controller model

---
+++ src/main/java/com/news/model/NewsRepository.java
@@ -0,0 +1,16 @@
+package com.news.model;
+
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NewsRepository extends JpaRepository<NewsVO, Integer> {
+    
+	Optional<NewsVO> findById(Integer newsId);
+	
+	Optional<NewsVO> findByTitle(String newsTitle);
+	
+}
