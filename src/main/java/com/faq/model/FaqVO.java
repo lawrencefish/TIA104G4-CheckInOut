a5cdd20e2e083163135a9591034e55ea2c72cb52
Brooke Lai <ming.peace@gmail.com>
2025-01-04 17:07:10 +0800

    faqVO

--- src/main/java/com/faq/model/FaqVO.java
+++ src/main/java/com/faq/model/FaqVO.java
@@ -1,5 +1,33 @@
 package com.faq.model;
 
-public class FaqVO {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "faq")
+public class FaqVO {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "faq_id")
+    private Integer faqId;
+    
+    @Column(name = "question", nullable = false)
+    private String question;
+    
+    @Column(name = "answer", nullable = false)
+    private String answer;
 }
