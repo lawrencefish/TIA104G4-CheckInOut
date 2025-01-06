603c57d25e15854f618d84cd69ee858f4994d029
Brooke Lai <ming.peace@gmail.com>
2025-01-06 11:13:35 +0800

    更新coupon vo

--- src/main/java/com/coupon/model/CouponVO.java
+++ src/main/java/com/coupon/model/CouponVO.java
@@ -11,10 +11,8 @@ import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
@@ -51,4 +49,77 @@ public class CouponVO {
 
     @Column(name = "discount_amount", nullable = false)
     private Integer discountAmount;
+
+	public Integer getCouponId() {
+		return couponId;
+	}
+
+	public void setCouponId(Integer couponId) {
+		this.couponId = couponId;
+	}
+
+	public LocalDateTime getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(LocalDateTime createTime) {
+		this.createTime = createTime;
+	}
+
+	public LocalDateTime getActiveDate() {
+		return activeDate;
+	}
+
+	public void setActiveDate(LocalDateTime activeDate) {
+		this.activeDate = activeDate;
+	}
+
+	public LocalDateTime getExpiryDate() {
+		return expiryDate;
+	}
+
+	public void setExpiryDate(LocalDateTime expiryDate) {
+		this.expiryDate = expiryDate;
+	}
+
+	public String getCouponName() {
+		return couponName;
+	}
+
+	public void setCouponName(String couponName) {
+		this.couponName = couponName;
+	}
+
+	public Integer getMinSpend() {
+		return minSpend;
+	}
+
+	public void setMinSpend(Integer minSpend) {
+		this.minSpend = minSpend;
+	}
+
+	public Integer getTravelCityNum() {
+		return travelCityNum;
+	}
+
+	public void setTravelCityNum(Integer travelCityNum) {
+		this.travelCityNum = travelCityNum;
+	}
+
+	public String getCouponDetail() {
+		return couponDetail;
+	}
+
+	public void setCouponDetail(String couponDetail) {
+		this.couponDetail = couponDetail;
+	}
+
+	public Integer getDiscountAmount() {
+		return discountAmount;
+	}
+
+	public void setDiscountAmount(Integer discountAmount) {
+		this.discountAmount = discountAmount;
+	}
+
 }
