23e4a705214b01e5fc63310b2ef33725a24fe2eb
figfang <ming.peace@gmail.com>
2025-01-09 18:33:45 +0800

    change

--- src/main/java/com/coupon/model/CouponService.java
+++ src/main/java/com/coupon/model/CouponService.java
@@ -8,6 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 @Service
 public class CouponService {
     
