b483480c8798b317ec95e6b8a581783c34c70a78
figfang <ming.peace@gmail.com>
2025-01-09 18:58:09 +0800

    Update CouponService.java

--- src/main/java/com/coupon/model/CouponService.java
+++ src/main/java/com/coupon/model/CouponService.java
@@ -8,8 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.creditcard.model.CreditcardVO;
-
 
 @Service
 public class CouponService {
