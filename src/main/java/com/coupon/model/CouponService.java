0b3398ef5b5fff4824900bc641c68c6564ce8c0c
figfang <ming.peace@gmail.com>
2025-01-09 18:53:41 +0800

    Update CouponService.java

--- src/main/java/com/coupon/model/CouponService.java
+++ src/main/java/com/coupon/model/CouponService.java
@@ -11,7 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class CouponService {
     
-	@Autowired
+    @Autowired
     private CouponRepository couponRepository;
 	
 	private SessionFactory sessionFactory;
