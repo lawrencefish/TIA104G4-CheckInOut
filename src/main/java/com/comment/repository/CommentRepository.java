d8cbe5f0aa27683038e113041ce088ff93df2933
Enzo Pan <enzopan1109@gmail.com>
2025-01-21 18:20:16 +0800

    客戶詳情顯示備註

--- src/main/java/com/comment/repository/CommentRepository.java
+++ src/main/java/com/comment/repository/CommentRepository.java
@@ -2,9 +2,17 @@ package com.comment.repository;
 
 import com.comment.model.Comment;
 import com.report.model.Reports;
+
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
+	@Query("SELECT c.commentContent FROM Comment c WHERE c.hotelId = :hotelId AND c.memberId = :memberId")
+	List<String> findCommentsByHotelIdAndMemberId(@Param("hotelId") Integer hotelId, @Param("memberId") Integer memberId);
 }
